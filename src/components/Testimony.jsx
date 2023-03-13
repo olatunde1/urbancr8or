fba21fc95e4f6e8143b7e165ea4d1f12c04936b2
Adetunji <idowuolatunde32@gmail.com>
2023-03-13 19:57:15 +0100

    project completed

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -48,11 +48,11 @@ function App() {
           }
           return (
             <article key={id} className={position}>
-              <img src={image} alt={name} className="person-img" />
+              {/* <img src={image} alt={name} className="person-img" /> */}
               <h4>{name}</h4>
               <p className="title">{title}</p>
               <p className="text">{quote}</p>
-              <FaQuoteRight className="icon" />
+              {/* <FaQuoteRight className="icon" /> */}
             </article>
           );
         })}
